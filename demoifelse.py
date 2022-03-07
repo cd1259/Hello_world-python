b55cae77394126873a43cfd424b9e981ec37820b
Bill <92319486+cd1259@users.noreply.github.com>
2022-03-07 15:08:58 +0800

    Add files via upload

---
+++ demoifelse.py
@@ -0,0 +1,13 @@
+num = int(input("输入整数->"))
+if num % 2 == 0:
+    print('偶数')
+else:
+    print('奇数')
+if 90 <= num <= 100:
+    print('A')
+if 80 <= num < 90:
+    print('B')
+elif 70 <= num < 80:
+    print('C')
+else:
+    print('test')
